f99e7bb3626c1696523bfccacdb31c27f90104b7
Masum Billah <masumbillahyusuf1621@gmail.com>
2023-02-03 14:52:38 +0600

    how to control array object

--- more-js-problems/cheapset.js
+++ more-js-problems/cheapset.js
@@ -7,7 +7,7 @@ const phones = [
     { name: 'oppo', camera: 21, storage: '32gb', price: 44000, color: 'silver' },
     { name: 'nokia', camera: 9, storage: '32gb', price: 30000, color: 'silver' },
     { name: 'htc', camera: 10, storage: '32gb', price: 62000, color: 'silver' }
-]
+];
 
 function cheapestPhone(phones) {
     let cheapest = phones[0];
@@ -37,10 +37,44 @@ function cheapestPhoneCamera(phones) {
 const mySelection2 = cheapestPhoneCamera(phones);
 // console.log(mySelection2);
 
-// calculate the total cost of the products in a shopping cart
+// 2. calculate the total cost of the products in a shopping cart
 const shoppingCart = [
     { name: 'shoe', price: 1100 },
     { name: 'shirt', price: 1700 },
     { name: 'pant', price: 3000 },
     { name: 'belt', price: 600 }
-]
+];
+
+function totalCost(products) {
+    let sum = 0;
+    for (let i = 0; i < products.length; i++) {
+        const product = products[i];
+        // console.log(product);
+        sum = sum + product.price;
+    }
+    return sum;
+}
+
+const expense = totalCost(shoppingCart);
+console.log('total expense today: ', expense);
+
+// 3. total cost with cart Quantity
+const shoppingCart2 = [
+    { name: 'shoe', price: 1100, quantity: 2 },
+    { name: 'shirt', price: 1700, quantity: 5 },
+    { name: 'pant', price: 3000, quantity: 4 },
+    { name: 'belt', price: 600, quantity: 3 },
+];
+
+function totalCost2(products) {
+    let sum = 0;
+    for (let i = 0; i < products.length; i++) {
+        const product = products[i];
+        const productTotal = product.price * product.quantity;
+        sum = sum + productTotal;
+    }
+    return sum;
+}
+
+const expense2 = totalCost2(shoppingCart2);
+console.log('total expense today: ', expense2);
